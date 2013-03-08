5184e35ba22d5e33b7fbdfbb6929623a5f5a33c6
wnls <wnlscindy@gmail.com>
2013-03-08 00:17:19 -0500

    display simple user stats in Home
    users info stored in routes/index.js
    need to be stored in lib/users/users.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,47 @@
+//var users = require('../lib/users');
+var users = [
+{name: 'Weini Yu', 
+ username: 'weini', 
+ email: 'weini@domain.com', 
+ password: 'weini_pw', 
+ location: 'weini_location', 
+ website: 'weini_website', 
+ profileImage: 'weini_image.jpg', 
+ follower: ['rae', 'yoshie'], 
+ following: ['rae', 'yoshie'], 
+ tweets: [000, 001] },
+{name: 'Rae Recto', 
+ username: 'rae', 
+ email: 'rae@domain.com', 
+ password: 'rae_pw', 
+ location: 'rae_location', 
+ website: 'rae_website', 
+ profileImage: 'rae_image.jpg', 
+ follower: ['weini', 'yoshie'], 
+ following: ['weini', 'yoshie'], 
+ tweets: [010, 011] },
+];
 
 /*
  * GET home page.
  */
 exports.home = function(req, res){
-	var id = req.params.id;
+	var username = req.params.id;
+	var i;
+	console.log(users.length);
+	for (i=0; i<users.length; i++) {
+		if (users[i].username == username) {
+			console.log(users[i].username);
+			break;
+		}
+	}
 	res.render('home', 
-  			{ title: 'Home Page',
-  			  user: 'Weini'//users[i].name,
-  			  //id: id //id,
-  			  //followerN: 2 //users[i].follower.length,
-  			  //followingN: 1// users[i].following.length,
-  			   }
-  			);
+  			{ title: 'Home',
+  			  name: users[i].name,
+  			  username: username,
+  			  followerN: users[i].follower.length,
+  			  followingN: users[i].following.length,
+  			   } );
 	
 };
 
