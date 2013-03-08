27c9694f8b14a4fa65455d33b9b3e37cf358c9f8
wnls <wnlscindy@gmail.com>
2013-03-07 23:51:28 -0500

    add views/home.ejs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
@@ -32,6 +32,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/form', routes.index);
+app.get('/:id/home', routes.home);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,18 @@
 /*
  * GET home page.
  */
+exports.home = function(req, res){
+	var id = req.params.id;
+	res.render('home', 
+  			{ title: 'Home Page',
+  			  user: 'Weini'//users[i].name,
+  			  //id: id //id,
+  			  //followerN: 2 //users[i].follower.length,
+  			  //followingN: 1// users[i].following.length,
+  			   }
+  			);
+	
+};
 
 exports.index = function(req, res){
   res.render('index', { title: 'Home' });
