d01cf8cdf8349b21df85ed2059debbd636792962
yoshie <ysasaki2014@gmail.com>
2013-03-12 13:11:34 -0400

    Working Project2

---
+++ routes/entry.js
@@ -0,0 +1,184 @@
+var users = require('../lib/users');
+var tweets = require('../lib/tweets');
+// Records the logged in user:
+var userids = 0;
+// A logged in "database":
+var online = {};
+exports.online = online; //needed if newTweet?
+// # User Server-Side Routes
+
+//GET rsegister
+exports.register = function(req, res){
+  res.render('register',{title: 'Tweetee', message: req.flash("error")});
+};
+
+//GET forgotlogin
+exports.forgotlogin = function(req, res){
+  res.render('forgotlogin',{title: 'Tweetee', message: ""});
+};
+
+/* 
+ * Post forgotlogin page with respective error message if 
+ *
+ */
+exports.forgotloginProcess = function(req, res){
+  var email = req.body.email;
+  users.lookupForgotLoginInfo(email, function(message) {
+      req.flash('message', message);
+      res.render('forgotlogin',{title: 'Tweetee', message: req.flash("message")});
+  });
+};
+
+exports.verifyCode = function(req, res){
+  res.render('verifyCode',{title: 'Tweetee', message: req.flash("message")});
+};
+
+//Registration
+exports.verify = function(req, res){
+    var name = req.body.name;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var retypeP = req.body.retypeP;
+    var location = req.body.location;
+    var website = req.body.website;
+    users.lookupRegistrationParams(name, username, email, password, retypeP, location, website, function(message) {
+        if(message){
+            req.flash('message', message);
+            res.render('register',{title: 'Tweetee', message: req.flash("message")});
+        }else{
+            req.flash('message', "A code has been sent out to the email provided."
+              +"Submit it to confirm the account and complete the registration.");
+            res.render('verifyCode',{title:"Tweetee", message: req.fregister.ejslash("message"), email:email}); 
+        }
+    });
+}
+
+exports.codeCheck = function(req,res){
+    users.lookupCodeCheck(req.body.code, function(message){
+        if(message){
+            req.flash('message', message);
+            res.render('verifyCode',{title:"Tweetee", message:req.flash("message")});
+        }else{
+            req.flash("msg", "The code is correct! Login to begin.");
+            res.redirect('/');
+        }
+  });
+}
+
+//Send the following to the client (user's page)
+exports.home = function (req, res) {
+    var userid=req.cookies.userid;
+    var onlineUser=online[userid];
+    console.log(req.params.user);
+    if (onlineUser.username == req.params.user){
+        var username = onlineUser.username;
+        var u = users.getUserById(username);
+        var j = tweets.tweets.length-1;
+        var content='';
+        // display timeline tweets
+        for (var i=j; i >= j-10 && i >= 0; i--) {
+    //console.log("i="+i);
+          var t = tweets.tweets[i];
+          var usr = users.getUserById(t.username);
+          var a = t.msg.split(" ");
+          console.log("split: "+a);
+          content += '<p><b>'+usr.name+'</b> <a href="/'+t.username+'/profile">@'+t.username+'</a><br>'
+                 +users.msgToHtml(t.msg)+'<br>'
+                 +t.date+'</p>';
+        }
+        res.render('home', 
+               { title: 'Home',
+                name: u.name,
+                username: username,
+                followerN: u.follower.length,
+                followingN: u.following.length,
+                tweets: content } );
+    }else{
+        res.send('Page Access Not Authorized.');
+    }
+};
+
+
+// ## login
+// Provides a user login/register view. Based on cookies example
+// in lecture 18 by Timothy Richards.
+exports.login = function(req, res){
+  // Grab any messages being sent to use from redirect.
+  var authmessage = users.flash(req, res, 'userAuth') || '';
+
+  // TDR: redirect if logged in:
+  var userid  = req.cookies.userid;
+  console.log("Cookie userid:"+req.cookies.userid);
+
+  // TDR: If the user is already logged in - we redirect to the
+  // main application view. We must check both that the `userid`
+  // and the `online[userid]` are undefined. The reason is that
+  // the cookie may still be stored on the client even if the
+  // server has been restarted.
+  if (userid !== undefined && online[userid] !== undefined) {
+    var onlineUser=online[userid] 
+    res.redirect('/'+onlineUser.username+'/home');
+  }
+  else {
+    // Render the login view if this is a new login.
+    res.render('login', { title   : 'Tweetee',
+                          message : authmessage,
+                          msg: req.flash("msg") });//For login username and password
+  }
+};
+
+// ## authentication
+// Performs **basic** user authentication. Based on cookies example
+// in lecture 18 by Timothy Richards. 
+exports.userAuth = function(req, res) {
+  // TDR: redirect if logged in:
+  var userid = req.cookies.userid;
+
+  // TDR: do the check as described in the `exports.login` function.
+  if (userid !== undefined && online[userid] !== undefined) {
+    var onlineUser=online[userid] 
+    res.redirect('/'+onlineUser.username+'/home');
+  }
+  else {
+    // Pull the values from the form.
+    var username = req.body.username;
+    var password = req.body.password;
+    // Perform the user lookup.
+    users.lookup(username, password, function(error, user) {
+      if (error) {
+        // If there is an error we "flash" a message to the
+        // redirected route `/user/login`.
+        users.flash(req, res, 'userAuth', error);
+        res.redirect('/');
+      }
+      else {
+        // TDR: use cookie to record stateful connection. Here
+        // we record the generated userid as a cookie to be
+        // passed back and forth between client and server.
+        userid = ++userids;
+        res.cookie('userid',
+                   userid+'',
+                   { maxAge : 900000 }); // 15 minutes
+        // Store the user in our in memory database.
+        online[userid] = user;
+        // Redirect to main.
+        var onlineUser=online[userid] 
+        res.redirect('/'+onlineUser.username+'/home');
+      }
+    });
+  }
+};
+
+// ## logout
+// Deletes user info & cookies - then redirects to login.
+exports.logout = function(req, res) {
+  // TDR: handle cookies
+  var userid = req.cookies.userid;
+  if (online[userid] !== undefined) {
+    res.clearCookie('userid');
+    delete online[userid];
+  }
+  res.redirect('/');
+};
+
--- routes/index.js
+++ routes/index.js
@@ -1,90 +1,42 @@
 var users = require('../lib/users');
-var tweets = require('../lib/tweets')
+var tweets = require('../lib/tweets');
+var entry = require('../routes/entry');
 
-var loggedInUser;
-var ul = users.ul;
+//var loggedInUser = "tim";
+//var userdb = users.userdb;
+var online = entry.online;
 /*
  * GET home page.
  */
 exports.home = function(req, res){
-	var username = req.params.id;
-  loggedInUser = username;
-  console.log(tweets.tweets);
-	//var i;
-	//console.log(username);
-  /*
-	for (i=0; i<ul.length; i++) {
-		if (ul[i].username === username) {
-			//console.log(ul[i].username);
-			loggedInUser = username;
-			break;
-		}
-	}*/
-  var u = users.getUserById(username);
-  //var t = users.getTByUser(username);
-  var j = tweets.tweets.length-1;
-  var content='';
-  // display timeline tweets
-  for (var i=j; i >= j-10 && i >= 0; i--) {
-    //console.log("i="+i);
-    var t = tweets.tweets[i];
-    var usr = users.getUserById(t.username);
-    //console.log(usr.name);
-    //console.log(i+" la");
-    // data.replace(/(.)/g, '<img src="$1.png" />')
-    //var htmlmsg = t.msg;
-    var a = t.msg.split(" ");
-    console.log("split: "+a);
-    content += '<p><b>'+usr.name+'</b> <a href="/'+t.username+'/profile">@'+t.username+'</a><br>'
-              //+t.msg+'<br>'
-              +msgToHtml(t.msg)+'<br>'
-              +t.date+'</p>';
-    //console.log("finish "+i);
+  var userid=req.cookies.userid;
+  var onlineUser=online[userid];
+  if ( onlineUser.username == req.params.id){
+    var u = users.getUserById(onlineUser.username);
+    //console.log("onlineUser "+u);
+    var tl = tweets.getRecentT(onlineUser, u.following, 20);
+    // display timeline tweets
+
+  	res.render('home', 
+    			{ title: 'Home',
+    			  name: u.name,
+    			  username: u.username,
+    			  followerN: u.follower.length,
+    			  followingN: u.following.length,
+            tweets: tweetsToHtml(tl)
+    			   } );
+  } else {
+        res.send('Page Access Not Authorized.');
   }
-	res.render('home', 
-  			{ title: 'Home',
-  			  name: u.name,
-  			  username: username,
-  			  followerN: u.follower.length,
-  			  followingN: u.following.length,
-          tweets: content
-  			   } );
-	
 }
 
-/**
- * Find @username and #hashtag in a tweet message and convert them to a html href link.
- * In order to be recognized as a @username mention, @ symbol must be the start
- * of a word. And @username@username is considered invalid and is ignored.
- * #hashtag starts with # and end before a space. #ford! is considered a hashtag.
- */
-function msgToHtml(msg) {
-  msg = msg.split(" ");
-  var content = '';
-  var len = msg.length;
-  for (var i=0; i < len; i++) {
-    var word = msg[i];
-    console.log("");
-    if (word.charAt(0) === "@" && word.split("\@").length === 2) {
-      content += ' <a href="/'+word.substring(1)+'/profile">'+word+'</a> ';
-    } else if (word.charAt(0) === "#" && word.split("\#").length === 2) { //#ford! <- !
-      content += ' <a href="/search/'+word.substring(1)+'">'+word+'</a> ';
-    } else {
-      content += word+" ";
-    } 
-  }
-  console.log("html content: "+content);
-  return content;
-}
 
 exports.newtweet = function(req, res) {
-
-  console.log(req.body.message);
-  tweets.addTweet(tweets.tweets.length, loggedInUser, req.body.message, null, null);
-  users.addUserT(loggedInUser, tweets.tweets.length-1);
-  console.log(tweets.tweets);
-  //console.log(users.getUserById(loggedInUser));
-  res.redirect('/'+loggedInUser+'/home');
+  var username = req.params.id;
+  var u = users.getUserById(username);
+  tweets.addTweet(tweets.tweetdb.length, u.name, u.username, req.body.message, null, null, null);
+  users.addUserT(u.username, tweets.tweetdb.length-1);
+  res.redirect('/'+username+'/home');
 }
 
 exports.id = function(req, res) {
@@ -95,22 +47,18 @@ exports.profile = function(req, res) {
   var username = req.params.id;
   var u = users.getUserById(username);
   if (u !== undefined ) {
-    var tl = tweets.getTByUser(username);
-    //console.log("profile tl: "+tl);
-    var j = tl.length-1;
-    //console.log(j);
+    var tl = tweets.getTByUser(username, 20);
+    var j = tl.length;
+
+    // display tweets
     var content='';
-    for (var i=j; i >= j-10 && i >= 0; i--) {
-      //console.log("i="+i);
+    for (var i=0; i < j; i++) {
       var t = tl[i];
       var usr = users.getUserById(t.username);
-      //console.log(usr.name);
-      //console.log(i+" la");
-      content += '<p><b>'+usr.name+'</b> <a href="/'+username+'">@'+username+'</a><br>'
+      content += '<p><b>'+usr.name+'</b> <a href="/'+username+'/profile">@'+username+'</a><br>'
                 //+t.msg+'<br>'
                 +msgToHtml(t.msg)+'<br>'
                 +t.date+'</p>';
-      //console.log("finish "+i);
     }
     res.render('profile',
               {title: 'Profile',
@@ -130,12 +78,7 @@ exports.profile = function(req, res) {
 
 exports.search = function(req, res) {
   var query = "#"+req.params.query;
-  var result = tweets.getTByHashtag(query);
-  console.log("query: "+query);
-  console.log("result: "+result[0]);
-  
-  //var u = users.getUserById(username);
-  //var t = users.getTByUser(username);
+  var result = tweets.getTByHashtag(query, 20);
   var j = result.length-1;
   var content='';
   // display timeline tweets
@@ -143,17 +86,11 @@ exports.search = function(req, res) {
     //console.log("i="+i);
     var t = result[i];
     var usr = users.getUserById(t.username);
-    //console.log(usr.name);
-    //console.log(i+" la");
-    // data.replace(/(.)/g, '<img src="$1.png" />')
-    //var htmlmsg = t.msg;
     var a = t.msg.split(" ");
-    console.log("split: "+a);
     content += '<p><b>'+usr.name+'</b> <a href="/'+t.username+'/profile">@'+t.username+'</a><br>'
               //+t.msg+'<br>'
               +msgToHtml(t.msg)+'<br>'
               +t.date+'</p>';
-    //console.log("finish "+i);
   }
   res.render('search',
              {title: 'Search Result',
@@ -168,12 +105,12 @@ exports.follower = function(req, res) {
   var user = users.getUserById(username);
   var followerlist = user.follower;
   var content = '';
-  console.log("followerlist: ",followerlist);
+  //console.log("followerlist: ",followerlist);
   if (followerlist.length !== 0) {
-    content += userToHtml(followerlist);
+    content += userToHtml(followerlist, "Delete");
 
   }
-  console.log("content: ", content);
+  //console.log("content: ", content);
 	res.render('follower', 
   			{ title: 'Follower',
   			  name: user.name,
@@ -187,12 +124,12 @@ exports.following = function(req, res) {
   var user = users.getUserById(username);
   var followinglist = user.following;
   var content = '';
-  console.log("followinglist: ",followinglist);
+  //console.log("followinglist: ",followinglist);
   if (followinglist.length !== 0) {
-    content += userToHtml(followinglist);
+    content += userToHtml(followinglist, "Unfollow");
 
   }
-  console.log("content: ", content);
+  //console.log("content: ", content);
   res.render('following', 
         { title: 'Following',
           name: user.name,
@@ -202,34 +139,79 @@ exports.following = function(req, res) {
 }
 
 exports.interaction = function(req, res) {
-  loggedInUser()
+  //console.log("loggedinuser: "+loggedInUser);
+  var username = req.params.id;
+  var user = users.getUserById(username);
+  var tl = tweets.getTByMention(username, 20);
+
+  res.render('interaction',
+            { title: 'Interaction',
+              name: user.name,
+              username: user.username,
+              tweets: tweetsToHtml(tl)
+              });
 
 }
+/*
+exports.to_interaction = function(req, res) {
+  res.redirect('/'+loggedInUser+'/interaction');
+}
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Home' });
-};
+exports.to_home = function(req, res){
+  res.redirect('/'+loggedInUser+'/home');
+};*/
 
-exports.form = function(req, res) {
 
-};
-
-function userToHtml(userlist) {
-  console.log("userlist: ", userlist);
+function userToHtml(userlist, btntext) {
+  //console.log("userlist: ", userlist);
   var content = '';
   var len = userlist.length-1;
-  console.log("len ",len);
+  //console.log("len ",len);
   for (var i=len; i >= 0; i--) {
-    console.log("userlist[i]: ",userlist[i]);
+    //console.log("userlist[i]: ",userlist[i]);
     var u = users.getUserById(userlist[i]);
-    console.log("u: ",u);
+    //console.log("u: ",u);
     content += '<p><b>'+u.name+'</b> <a href="/'+u.username+'/profile">@'+u.username+'</a><br>';
-    content += '<button onclick="deleteFollower()">Delete</button></p>';
+    content += '<button onclick="deleteFollower()">'+btntext+'</button></p>';
   }
   return content;
 }
 
-function deleteFollower() {
-  console.log("click");
-  redirect("/weini/home");
+function tweetsToHtml(tl) {
+  var j = tl.length;
+  var content='';
+  for (var i=0; i < j; i++) {
+    var t = tl[i];
+    var usr = users.getUserById(t.username);
+    var a = t.msg.split(" ");
+    content += '<p><b>'+t.name+'</b> <a href="/'+t.username+'/profile">@'+t.username+'</a><br>'
+              //+t.msg+'<br>'
+              +msgToHtml(t.msg)+'<br>'
+              +t.date+'</p>';
+  }
+  return content;
+}
+/**
+ * Find @username and #hashtag in a tweet message and convert them to a html href link.
+ * In order to be recognized as a @username mention, @ symbol must be the start
+ * of a word. And @username@username is considered invalid and is ignored.
+ * #hashtag starts with # and end before a space. #ford! is considered a hashtag.
+ */
+function msgToHtml(msg) {
+  msg = msg.split(" ");
+  var content = '';
+  var len = msg.length;
+  for (var i=0; i < len; i++) {
+    var word = msg[i];
+    //console.log("");
+    if (word.charAt(0) === "@" && word.split("\@").length === 2) {
+      content += ' <a href="/'+word.substring(1)+'/profile">'+word+'</a> ';
+    } else if (word.charAt(0) === "#" && word.split("\#").length === 2) { //#ford! <- !
+      content += ' <a href="/search/'+word.substring(1)+'">'+word+'</a> ';
+    } else {
+      content += word+" ";
+    } 
+  }
+  //console.log("html content: "+content);
+  return content;
 }
